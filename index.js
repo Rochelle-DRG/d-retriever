ef76e202eee2011b3f31ce464739a25b1c6813fc
R Wolfe <rochelle.wolfe@davey.com>
2019-11-05 15:38:43 -0500

    that was an utter failure

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-let output = document.getElementById('output');
-
-require([
-    "esri/Map",
-    "esri/views/MapView",
-    "esri/layers/FeatureLayer",
-    "esri/layers/GraphicsLayer",
-    "esri/Graphic"
-],
-    function (Map, MapView, FeatureLayer, GraphicsLayer, Graphic) {
-
-        var map = new Map({
-            basemap: "gray"
-        });
-
-        var view = new MapView({
-            container: "viewDiv",
-            map: map,
-            center: [-81.357502, 41.154396],
-            zoom: 13
-        });
-
-        // Reference the feature layer to query
-        var featureLayer = new FeatureLayer({
-            url: "https://services3.arcgis.com/kwmUh9MJciUcuce3/arcgis/rest/services/MyMapService/FeatureServer/0?token=g6SmFR5yRa07hhE7Rn5UzNBK8YghiP9oixoMnIrN8_hpA3G42asSjxgl0e5wjJeRXUSjlQNx087st9EFE68wVtRMAUDmpkyaJHLW8vN7SRDZpvw9lTm-jwUCvzkKWrNsJ6wR-t3nn2dG9yCcJDnSCp9ziB_krdlVlyUpvz82iWyEpzhMPyAFlDINWSlZeY2Y60_YPFj58u3SEbyu4XzIw5_jBsCvPe6myJGb4eL1wf0hu8ZHtcPFAZ9ITMGZeTKH",
-        });
-
-        // Layer used to draw graphics returned
-        var graphicsLayer = new GraphicsLayer();
-        map.add(graphicsLayer);
-
-    });// end require main function
+console.log("index loaded");
+$(document).ready(function () {
+    console.log("document ready, jquery working");
+    $("button").click(function () {
+        $("#div1").load("demo_test.txt");
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://www.arcgis.com/sharing/rest/oauth2/token?client_id=Cgx3mZ18s8C3bqGs&client_secret=dcf1f44ea97345a99dc4695284624000&grant_type=client_credentials",
+            "method": "POST",
+            "headers": {
+              "User-Agent": "PostmanRuntime/7.19.0",
+              "Accept": "*/*",
+              "Cache-Control": "no-cache",
+              "Postman-Token": "5bb794d8-6c70-4784-8f2c-792a2d054184,b366cca4-5b88-4ebd-9915-a23b01d0cd1d",
+              "Host": "www.arcgis.com",
+              "Accept-Encoding": "gzip, deflate",
+              "Content-Length": "0",
+              "Connection": "keep-alive",
+              "cache-control": "no-cache"
+            }
+          }
+          
+          $.ajax(settings).done(function (response) {
+            console.log(response);
+          });
+    });
+});
