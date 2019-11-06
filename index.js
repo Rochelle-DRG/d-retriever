c09fc8617ce63335251dd10458fe3c81852c43f3
R Wolfe <rochelle.wolfe@davey.com>
2019-11-06 15:58:41 -0500

    simple api call that returns an access token

--- index.js
+++ index.js
@@ -3,26 +3,25 @@ $(document).ready(function () {
     console.log("document ready, jquery working");
     $("button").click(function () {
         $("#div1").load("demo_test.txt");
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://www.arcgis.com/sharing/rest/oauth2/token?client_id=Cgx3mZ18s8C3bqGs&client_secret=dcf1f44ea97345a99dc4695284624000&grant_type=client_credentials",
-            "method": "POST",
-            "headers": {
-              "User-Agent": "PostmanRuntime/7.19.0",
-              "Accept": "*/*",
-              "Cache-Control": "no-cache",
-              "Postman-Token": "5bb794d8-6c70-4784-8f2c-792a2d054184,b366cca4-5b88-4ebd-9915-a23b01d0cd1d",
-              "Host": "www.arcgis.com",
-              "Accept-Encoding": "gzip, deflate",
-              "Content-Length": "0",
-              "Connection": "keep-alive",
-              "cache-control": "no-cache"
-            }
-          }
-          
-          $.ajax(settings).done(function (response) {
-            console.log(response);
-          });
-    });
+
+
+        /**## This will actually return an access token ##**/
+        $.ajax({
+            method: "POST",
+            url: "https://www.arcgis.com/sharing/rest/oauth2/token?client_id=Cgx3mZ18s8C3bqGs&client_secret=dcf1f44ea97345a99dc4695284624000&grant_type=client_credentials",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            // data: { username: "John", password: "Boston" }
+        })
+        .done(function( msg ) {
+          console.log(msg);
+          document.write("access token is : <br/>" +msg.access_token);
+
+        })
+        .fail(function( jqXHR, textStatus ) {
+          alert( "Request failed: " + textStatus );
+        });
+
+
+    });//end on click
 });
