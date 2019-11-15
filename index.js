b814a45bc968adff6edc1ab6884de628bbc9d84f
R Wolfe <rochelle.wolfe@davey.com>
2019-11-15 15:49:05 -0500

    all Collector points now successfull load into MRK

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ $(document).ready(function () {
     var token;
     var messageDiv = document.getElementById('message')
 
-    /**Dummy Project details for the sample arcgis app I made*/
-    // var clientId = 'Cgx3mZ18s8C3bqGs';
-    // var clientSecret = 'dcf1f44ea97345a99dc4695284624000';
-
     /**Kent Ohio Stormwater sample argis app that Holly made */
     var clientId = 'Lv925FWYHn6Tg1ga'; // 
     var clientSecret = '7e442cc80b974ab6a94f3bd3d7e67f18'; // 
@@ -28,7 +24,6 @@ $(document).ready(function () {
     //     messageDiv.innerHTML = "login token is : <br/>" + token;
     //     messageDiv.innerHTML = "Okay, you're logged in. Click to get data";
     // }
-
 
     // /**## This will return an access token without the user logging in (uses clientID and secretID) ##**/
     // messageDiv.innerHTML = "Logging you in";
@@ -68,7 +63,7 @@ $(document).ready(function () {
             // console.log(msg);
             // messageDiv.innerHTML = "access token is : <br/>" + msg.token
             token = msg.token;
-            messageDiv.innerHTML = "Okay, we're logged in.";
+            messageDiv.innerHTML = "Okay, we're logged in to ArcGIS Online.";
         })
         .fail(function (jqXHR, textStatus) {
             alert("Request failed: " + textStatus);
@@ -76,37 +71,6 @@ $(document).ready(function () {
         });
 
     /**#######################   GETTING DATA FROM COLLECTOR    #########################**/
-    /** Query ALL the features from the whole TEST API */
-    $("#get-test-data").click(function () {
-        messageDiv.innerHTML = "Let's get some data";
-        $.ajax({
-            method: "POST",
-            url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0/query",
-            // token: token,
-            dataType: "json",
-            data: {
-                // returnCountOnly: "true",
-                f: "json",
-                // where: "PARK_NAME = 'National Parks Service' AND ELEV_FT > 250 and ELEV_FT < 1000",
-                where: "1=1",
-                outSr: "4326",
-                // outfields: "TRL_NAME,ELEV_FT,CITY_JUR,PARK_NAME,FID"
-                outfields: "*"
-            }
-        }) //end .ajax
-            .done(function (msg) {
-                console.log(msg);
-                var jsonInfo = JSON.stringify(msg);
-                // messageDiv.innerHTML = "Data: <br>" + msg;
-                messageDiv.innerHTML = "Data: <br>" + jsonInfo;
-            })
-            .fail(function (jqXHR, textStatus) {
-                alert("Request failed: " + textStatus);
-                messageDiv.innerHTML = "Ut-oh, data fetch failed.";
-            });
-        messageDiv.innerHTML = "That's all";
-    });//end on click
-
 
     /** Query ALL the features from the Kent Stormwater */
     $("#get-data").click(function () {
@@ -130,22 +94,29 @@ $(document).ready(function () {
         }) //end .ajax
             .done(function (msg) {
                 // console.log(msg);
+                messageDiv.innerHTML = "We have retrieved the data from collector. ";
+                console.log();
                 var jsonInfoAsString = JSON.stringify(msg.features);
                 // messageDiv.innerHTML = "Data: <br>" + jsonInfoAsString;
                 // messageDiv.innerHTML = "Data (string) length: <br>" + jsonInfoAsString.length;
                 // messageDiv.innerHTML = "Data : <br>" + JSON.stringify(msg.features[0]);
                 // messageDiv.innerHTML = "Data: <br>" + JSON.stringify(msg.features[0].attributes.OBJECTID);
-                // messageDiv.innerHTML = "Data: <br>" + msg.features[0].attributes.OBJECTID; //successfully returns the property
+                // messageDiv.innerHTML = "Data: <br>" + msg.features[1].attributes.OBJECTID; //successfully returns the property
 
                 /** For each manhole */
                 for (let i = 0; i < msg.features.length; i++) {
+                // for (let i = 0; i < 11; i++) {
+    
                     let stormManhole = makeManhole(msg.features[i]); //returns MRK-ready manhole object
-                    // submitToMRK(stormManhole);
-                }
+                    submitToMRK(stormManhole, i);
+                    // console.log(msg.features[i].attributes.OBJECTID+" : "+msg.features[i].attributes.GlobalID);
+                }//end For
+                // // /** TEST TEST TEST */
+                // let stormManholeTest = makeManhole(msg.features[7]); //returns MRK-ready manhole object
+                // // messageDiv.innerHTML = JSON.stringify(msg.features[1]) +"<br/><br/>"+JSON.stringify(msg.features[0])+"<br/><br/>"+JSON.stringify(msg.features[2]);
+                // messageDiv.innerHTML = "What I made: <br/>"+JSON.stringify(stormManholeTest) +"<br/><br/>"+"What Collector gave me: <br/>"+JSON.stringify(msg.features[7]) ;
 
-
-
-
+                // submitToMRK(stormManholeTest);
             })
             .fail(function (jqXHR, textStatus) {
                 alert("Request failed: " + textStatus);
@@ -164,102 +135,162 @@ $(document).ready(function () {
                 WRK_REGION: "N\/A",
                 WRK_AREA: "N/A",
                 UNIQUEID: collectedManhole.attributes.GlobalID,
-                NOTES: collectedManhole.attributes.Comments,
+                NOTES: nullToString(collectedManhole.attributes.Comments),
                 ChangeTIME: formatTime(collectedManhole.attributes.EditDate),
                 Inv_Time: formatTime(collectedManhole.attributes.CreationDate),
                 ChangeDATE: formatDate(collectedManhole.attributes.EditDate),
                 Inv_Date: formatDate(collectedManhole.attributes.CreationDate),
                 Status: collectedManhole.attributes.Site_Status,
                 MH_ID: "",
-                Elevation: 0, //collectedManhole.attributes.TopElevationft,
-                Inverts: collectedManhole.attributes.NumberInverts,
+                Elevation: convertNullElevation(collectedManhole.attributes.TopElevationft),
+                Inverts: convertNumInverts(collectedManhole.attributes.NumberInverts),
                 Location: "",
-                DT_Verify: formatDate(collectedManhole.attributes.EditDate),
-                Material: collectedManhole.attributes.MaterialMH,
-                Invert_1: collectedManhole.attributes.Invert1,
-                Invert_2: collectedManhole.attributes.Invert2,
-                Invert_3: collectedManhole.attributes.Invert3,
-                Invert_4: collectedManhole.attributes.Invert4,
-                Invert_5: collectedManhole.attributes.Invert5,
-                Invert_6: collectedManhole.attributes.Invert6,
-                Invert_7: collectedManhole.attributes.Invert7,
-                Invert_8: collectedManhole.attributes.Invert8,
-                FormInv: collectedManhole.attributes.FormedInvert,
-                Steps: collectedManhole.attributes.Steps,
-                SolLid: collectedManhole.attributes.SolidLid
+                DT_Verify: formatDTVerify(collectedManhole.attributes.EditDate),
+                Material: nullToString(collectedManhole.attributes.MaterialMH),
+                Invert_1: nullToString(collectedManhole.attributes.Invert1),
+                Invert_2: nullToString(collectedManhole.attributes.Invert2),
+                Invert_3: nullToString(collectedManhole.attributes.Invert3),
+                Invert_4: nullToString(collectedManhole.attributes.Invert4),
+                Invert_5: nullToString(collectedManhole.attributes.Invert5),
+                Invert_6: nullToString(collectedManhole.attributes.Invert6),
+                Invert_7: nullToString(collectedManhole.attributes.Invert7),
+                Invert_8: nullToString(collectedManhole.attributes.Invert8),
+                FormInv: nullToUnassigned(collectedManhole.attributes.FormedInvert),
+                Steps: nullToUnassigned(collectedManhole.attributes.Steps),
+                SolLid: nullToString(collectedManhole.attributes.SolidLid)
+
+                // ,UNIQUEID: "ShLa20191108153127",
+                // NOTES: "",
+                // ChangeTIME: "15:31:48",
+                // Inv_Time: "15:31:27",
+                // ChangeDATE: "11-08-2019",
+                // Inv_Date: "11-08-2019",
+                // Status: "Existing",
+                // MH_ID: "",
+                // Elevation: 0,
+                // Inverts: "0",
+                // Location: "",
+                // DT_Verify: 20191108,
+                // Material: "Unassigned",
+                // Invert_1: "",
+                // Invert_2: "",
+                // Invert_3: "",
+                // Invert_4: "",
+                // Invert_5: "",
+                // Invert_6: "",
+                // Invert_7: "",
+                // Invert_8: "",
+                // FormInv: "Unassigned",
+                // Steps: "Unassigned",
+                // SolLid: "Yes"
+    
             },
             geometry: {
                 type: "point",
                 coordinates: [
                     collectedManhole.geometry.x,
                     collectedManhole.geometry.y,
+
+                    // -9058101.08891544,
+                    // 5036721.17612163
+    
                 ]
             },
             factype: "Storm Manholes"
+
         };
 
         return manhole;
-    } // end makeManhole
+    }; // end makeManhole
 
     function formatDate(collectorDate) {
         let d = new Date(collectorDate);
-        let formattedDate = d.getMonth() + "-" + d.getDate() + "-" + d.getFullYear();
+        let formattedDate = ( "0"+(d.getMonth()+1) ).slice(-2) + "-" + ( "0"+d.getDate() ).slice(-2) + "-" + d.getFullYear();
         return formattedDate;
-    }
+    };
     function formatTime(collectorDate) {
         let d = new Date(collectorDate);
-        let formattedTime = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+        let formattedTime = ( "0"+(d.getHours()) ).slice(-2) + ":" + ( "0"+(d.getMinutes()) ).slice(-2) + ":" + ( "0"+(d.getSeconds()) ).slice(-2);
         return formattedTime;
+    };
+    function formatDTVerify(collectorDate){
+        let d = new Date(collectorDate);
+        let formattedDate = d.getFullYear() + "" + ( "0"+(d.getMonth()+1) ).slice(-2) + "" + ( "0"+d.getDate() ).slice(-2);
+        return Number(formattedDate);
+    };
+    function nullToString(value){
+        if (value === null){
+            return "";
+        }
+        else return value;
+    };
+    function convertNullElevation(value){
+        if (value === null){
+            return 0;
+        }
+        else return value;
+    };
+    function convertNumInverts(value){
+        if (value === null){
+            return "0";
+        }
+        else return value;
+    };
+    function nullToUnassigned(value){
+        if (value === null){
+            return "Unassigned";
+        }
+        else return value;
     }
 
 
 
     /**#######################   SUBMITTING DATA TO MRK    #########################**/
 
-    let testManhole = {
-        properties: {
-            WRK_REGION: "N\/A",
-            WRK_AREA: "N/A",
-            UNIQUEID: "ShLa20191108153127",
-            NOTES: "",
-            ChangeTIME: "15:31:48",
-            Inv_Time: "15:31:27",
-            ChangeDATE: "11-08-2019",
-            Inv_Date: "11-08-2019",
-            Status: "Existing",
-            MH_ID: "",
-            Elevation: 0,
-            Inverts: "0",
-            Location: "",
-            DT_Verify: 20191108,
-            Material: "Unassigned",
-            Invert_1: "",
-            Invert_2: "",
-            Invert_3: "",
-            Invert_4: "",
-            Invert_5: "",
-            Invert_6: "",
-            Invert_7: "",
-            Invert_8: "",
-            FormInv: "Unassigned",
-            Steps: "Unassigned",
-            SolLid: "Yes"
-        },
-        geometry: {
-            type: "Point",
-            coordinates: [
-                -9058101.08891544,
-                5036721.17612163
-            ]
-        },
-        factype: "Storm Manholes"
-    };
-    let testManhole2 = JSON.parse('{"properties":{"WRK_REGION":"N\/A","WRK_AREA":"N/A","UNIQUEID":"ShLa20191108153127","NOTES":"","ChangeTIME":"15:31:48","Inv_Time":"15:31:27","ChangeDATE":"11-08-2019","Inv_Date":"11-08-2019","Status":"Existing","MH_ID":"","Elevation":0,"Inverts":"0","Location":"","DT_Verify":20191108,"Material":"Unassigned","Invert_1":"","Invert_2":"","Invert_3":"","Invert_4":"","Invert_5":"","Invert_6":"","Invert_7":"","Invert_8":"","FormInv":"Unassigned","Steps":"Unassigned","SolLid":"Yes"}, "geometry":{"type":"Point","coordinates":[-9058101.08891544,5036721.17612163]},"factype":"Storm Manholes"}');
+    // let testManhole = {
+    //     properties: {
+    //         WRK_REGION: "N\/A",
+    //         WRK_AREA: "N/A",
+    //         UNIQUEID: "ShLa20191108153127",
+    //         NOTES: "",
+    //         ChangeTIME: "15:31:48",
+    //         Inv_Time: "15:31:27",
+    //         ChangeDATE: "11-08-2019",
+    //         Inv_Date: "11-08-2019",
+    //         Status: "Existing",
+    //         MH_ID: "",
+    //         Elevation: 0,
+    //         Inverts: "0",
+    //         Location: "",
+    //         DT_Verify: 20191108,
+    //         Material: "Unassigned",
+    //         Invert_1: "",
+    //         Invert_2: "",
+    //         Invert_3: "",
+    //         Invert_4: "",
+    //         Invert_5: "",
+    //         Invert_6: "",
+    //         Invert_7: "",
+    //         Invert_8: "",
+    //         FormInv: "Unassigned",
+    //         Steps: "Unassigned",
+    //         SolLid: "Yes"
+    //     },
+    //     geometry: {
+    //         type: "Point",
+    //         coordinates: [
+    //             -9058101.08891544,
+    //             5036721.17612163
+    //         ]
+    //     },
+    //     factype: "Storm Manholes"
+    // };
+    // let testManhole2 = JSON.parse('{"properties":{"WRK_REGION":"N\/A","WRK_AREA":"N/A","UNIQUEID":"ShLa20191108153127","NOTES":"","ChangeTIME":"15:31:48","Inv_Time":"15:31:27","ChangeDATE":"11-08-2019","Inv_Date":"11-08-2019","Status":"Existing","MH_ID":"","Elevation":0,"Inverts":"0","Location":"","DT_Verify":20191108,"Material":"Unassigned","Invert_1":"","Invert_2":"","Invert_3":"","Invert_4":"","Invert_5":"","Invert_6":"","Invert_7":"","Invert_8":"","FormInv":"Unassigned","Steps":"Unassigned","SolLid":"Yes"}, "geometry":{"type":"Point","coordinates":[-9058101.08891544,5036721.17612163]},"factype":"Storm Manholes"}');
 
-    submitToMRK(testManhole);
+    // submitToMRK(testManhole2);
 
-    function submitToMRK(manhole) {
-        console.log("submitting to TK");
+    function submitToMRK(manhole, manNum) {
+        messageDiv.innerHTML = "Submitting number "+manNum+" to MRK";
 
         let dataVar = {
             // f: "json",
@@ -278,17 +309,36 @@ $(document).ready(function () {
             data: JSON.stringify(dataVar)
         })
             .done(function (msg) {
-                console.log("success");
+                messageDiv.innerHTML = "Success for number: "+ manNum;
+                displaySuccess(manhole, manNum);
+
             })
             .fail(function (jqXHR, textStatus) {
                 console.log("Submit request failed: " + textStatus);
+                console.log(manNum);
+                console.log(manhole);
                 messageDiv.innerHTML = "Ut-oh, submitting failed. "+ textStatus;
+                displayFailed(manhole, manNum);
             })
 
 
 
     }
+/**#######################   DISPLAYING SUCCESS OR FAIL RESULTS    #########################**/
+    var failNode = document.getElementById("failures");
+    var sNode = document.getElementById("successes");
 
+    function displayFailed(manhole, number){
+        let failLI = document.createElement("li");
+        failLI.innerText = number + ": " + JSON.stringify(manhole);
+        failNode.appendChild(failLI);
+    };
+
+    function displaySuccess(manhole, number){
+        let successLI = document.createElement("li");
+        successLI.innerText = number + ": " + JSON.stringify(manhole);
+        sNode.appendChild(successLI);
+    };
 
 
 });//end document ready
